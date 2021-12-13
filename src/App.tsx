63d079e5aa34ca4340e8f2f83e0453a79a2ce637
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2021-12-13 11:17:00 -0300

    Componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import { Header } from "./components/Header";
 import { GlobalStyles } from "./sytles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-
+    <>
+      <Header />
       <GlobalStyles />
-    </div>
+    </>
   );
 }
