5856a81a5ab2567174db8288f0e518ec29d1f21e
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2021-12-14 11:18:45 -0300

    Estrutura do Formulario

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import { Container } from "./style";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +11,23 @@ export function NewTransactionModal({
   onRequestOpen,
 }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestOpen}>
-      <h2>Cadastrar Transação</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestOpen}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
+        <h2>Cadastrar Transação</h2>
+
+        <input placeholder="Título" />
+
+        <input type="number" placeholder="Valor" />
+
+        <input placeholder="Categoria" />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
