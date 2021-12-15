06c8ed2eb79af8c4b95dc7050054b8069de1956e
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2021-12-15 10:45:13 -0300

    Carregando trasaçoes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyles } from "./sytles/global";
-import { TransactionsContext } from "./TransactionsContext";
+import { TransactionProvider } from "./TransactionsContext";
 
 Modal.setAppElement("#root");
 
@@ -21,7 +21,7 @@ export function App() {
   }
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -31,6 +31,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyles />
-    </TransactionsContext.Provider>
+    </TransactionProvider>
   );
 }
